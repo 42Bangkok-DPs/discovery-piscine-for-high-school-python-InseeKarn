e86c74cbcfb1be33c0e3cb4129fa8d1fbe8e4409
InseeKarn <93970325+InseeKarn@users.noreply.github.com>
2024-10-07 22:45:21 +0700

    Create advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,12 @@
+num = 0
+
+while num <= 10:
+    j = 0
+    table = []
+
+    while j <= 10:
+        table.append(num * j)
+        j += 1
+    print(f"table de {num} : {' '.join(map(str, table))}")
+
+    num += 1
