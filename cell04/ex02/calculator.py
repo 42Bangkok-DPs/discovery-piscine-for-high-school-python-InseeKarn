5a086726ec207205a24249587f9bf291e866fd3d
InseeKarn <93970325+InseeKarn@users.noreply.github.com>
2024-10-07 23:24:01 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+import math
+fm = int(input("Give me the first number: "))
+sm = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{fm} + {sm} = {fm + sm}")
+print(f"{fm} - {sm} = {fm - sm}")
+print(f"{fm} / {sm} = {math.ceil(fm / sm)}")
+print(f"{fm} * {sm} = {fm * sm}")
