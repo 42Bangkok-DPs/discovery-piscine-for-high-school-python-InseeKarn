25cf2739313c71faff13873f3b13601aed268740
Karnphong Saratontiphong <ksaraton@z2t15c4.42bangkok.com>
2024-10-08 18:13:14 +0700

    chmod all file

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -4,4 +4,4 @@ table1 = [2, 8, 9, 48, 8, 22, -12, 2]
 table2 = numpy.array(table1)
 
 print("Original array: ", table1)
-print ("New array: " + "[" + ", ".join(map(str, table2 * 2)) + "]")
+print ("New array: " + "[" + ", ".join(map(str, table2 + 2)) + "]")
