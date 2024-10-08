208f72cb2ea9d7afb0af49b35df9d32fb148bdfe
Karnphong Saratontiphong <ksaraton@z2t15c4.42bangkok.com>
2024-10-08 20:51:44 +0700

    5.2

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -2,10 +2,13 @@
 
 import numpy
 table1 = [2, 8, 9, 48, 8, 22, -12, 2]
-table2 = numpy.array(table1) > 5
+table2 = []
 print("Original array: ", table1)
 
-res = list(numpy.where())
+for num in table1:
+    newnum = num
+    newnum += 2
+    if newnum >= 5:
+        table2.append(newnum)
 
-
-# print ("New array: " + "[" + ", ".join(map(str, table2 + 2)) + "]")
+print(table2)
